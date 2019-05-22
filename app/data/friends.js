4ed0b2bc99c9d9b018fbcf84f64a91354d003d3e
Eleani Pita <eleanipita@MacBook-Pro-de-Miguel.local>
2019-05-22 13:21:26 -0400

    files

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Eli Pita',
+      profilePic: '../images/gettyimages-452518574.jpg',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
